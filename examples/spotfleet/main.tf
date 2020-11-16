7efcd3ad58745aab5ca015047dacc0e5b8cc31ea
Ben Fortuna <bfortuna@odecee.com.au>
2020-11-16 12:53:07 +1100

    Enabled service discovery for example

--- examples/spotfleet/main.tf
+++ examples/spotfleet/main.tf
@@ -2,4 +2,5 @@ module "cluster" {
   source = "../.."
 
   name = var.name
+  servicediscovery_enabled = var.servicediscovery_enabled
 }
--- examples/spotfleet/vars.tf
+++ examples/spotfleet/vars.tf
@@ -1,3 +1,8 @@
 variable "name" {
   description = "Cluster name"
 }
+
+variable "servicediscovery_enabled" {
+  description = "Create service discovery private zone for the cluster"
+  default     = false
+}

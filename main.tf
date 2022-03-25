575b6347f949af97a1639953b2add49ef033e211
Ben Fortuna <bfortuna@odecee.com.au>
2022-03-25 16:48:25 +1100

    Updated capacity provider config (fixes issue with destroy)

--- main.tf
+++ main.tf
@@ -6,16 +6,7 @@
  * ![AWS ECS Cluster](aws_ecs_cluster.png)
  */
 resource "aws_ecs_cluster" "cluster" {
-  name               = var.name
-  capacity_providers = var.capacity_providers
-
-  dynamic "default_capacity_provider_strategy" {
-    for_each = var.default_capacity_provider != null ? [1] : []
-    content {
-      capacity_provider = var.default_capacity_provider
-      weight            = 100
-    }
-  }
+  name = var.name
 
   #checkov:skip=CKV_AWS_65:Dynamic container insights
   setting {
@@ -31,4 +22,17 @@ resource "aws_ecs_cluster" "cluster" {
       "figurate:s"    = "figurate/ecs-cluster/aws"
     },
   )
+}
+
+resource "aws_ecs_cluster_capacity_providers" "capacity_providers" {
+  cluster_name       = aws_ecs_cluster.cluster.name
+  capacity_providers = var.capacity_providers
+
+  dynamic "default_capacity_provider_strategy" {
+    for_each = var.default_capacity_provider != null ? [1] : []
+    content {
+      capacity_provider = var.default_capacity_provider
+      weight            = 100
+    }
+  }
 }
